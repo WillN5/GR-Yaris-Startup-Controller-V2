893fb6e93f32782aa2ca4d8980426af7b7283a92
Will Norman <will.norman@sky.com>
2025-04-06 08:07:58 +0100

    Update Code_Startup_Controller_v2.ino

--- Code/Code_Startup_Controller_v2/Code_Startup_Controller_v2.ino
+++ Code/Code_Startup_Controller_v2/Code_Startup_Controller_v2.ino
@@ -4,34 +4,34 @@
 // For use with PCB v2.0
 
 // Define all output pins that drive car button presses
-#define SWITCH_MODE 10   // sport / track
+#define SWITCH_MODE 10   // sport / track (slide switch determines which chassis signal wire is pulled low)
 #define SWITCH_LDA  14   // lane departure alert
 #define SWITCH_ASS  15   // auto stop start
 #define SWITCH_IMT  16   // iMT
 #define SWITCH_ESC  17   // ESC / traction
 
-// Define all output pins of indicator LEDs
-#define LED_SPORT   4
-#define LED_TRACK   3
-#define LED_LDA     2
-#define LED_ASS     1
-#define LED_IMT     0
-#define LED_ESC     18
+// Define all output pins of indicator LEDs (flash corresponding LED when button is pressed)
+#define LED_SPORT   4   // sport mode led
+#define LED_TRACK   3   // track mode led
+#define LED_LDA     2   // lane departure led
+#define LED_ASS     1   // auto stop/start led
+#define LED_IMT     0   // imt led
+#define LED_ESC     18  // esc led
 
-// Define input pins
-#define INPUT_ESC   5   // Input to determine ESC switch state - short (expert), long (all off)
+// Define input pins (connected to slide switches)
+#define INPUT_ESC   5   // Input to determine ESC switch state - expert mode or esc fully off
 #define INPUT_MODE  6   // Input to determine track/sport switch state
 
 void setup() {
 
-    // Configure output pins for button 'pressers'
+    // Configure pins as outputs for button 'pressers'
     pinMode(SWITCH_MODE, OUTPUT);
     pinMode(SWITCH_LDA, OUTPUT);
     pinMode(SWITCH_ASS, OUTPUT);
     pinMode(SWITCH_IMT, OUTPUT);
     pinMode(SWITCH_ESC, OUTPUT);
     
-    // Configure output pins for indicator LEDs
+    // Configure pins as outputs for indicator LEDs
     pinMode(LED_TRACK, OUTPUT);
     pinMode(LED_SPORT, OUTPUT);
     pinMode(LED_LDA, OUTPUT);
@@ -39,7 +39,7 @@ void setup() {
     pinMode(LED_IMT, OUTPUT);
     pinMode(LED_ESC, OUTPUT); 
 
-    // Ensure all pins are driven low
+    // Ensure all output pins are driven low
     digitalWrite(SWITCH_MODE, LOW);
     digitalWrite(SWITCH_LDA, LOW);
     digitalWrite(SWITCH_ASS, LOW);
@@ -53,59 +53,70 @@ void setup() {
     digitalWrite(LED_IMT, LOW);
     digitalWrite(LED_ESC, LOW);
 
-    // Configure input pins
+    // Configure input pins (slide switch inputs)
     pinMode(INPUT_ESC, INPUT);
     pinMode(INPUT_MODE, INPUT);
 
-    // Delay to allow car screens to load fully (large delay not necessary for function)
+    // Delay 3s to allow car to startup
     delay(3000);
 
     // Read states of inputs
-    bool STATE_ESC = digitalRead(INPUT_ESC);    // HIGH for expert (short), LOW for ESC fully off (long)
+    bool STATE_ESC = digitalRead(INPUT_ESC);    // HIGH for expert (short press), LOW for ESC fully off (long press)
     bool STATE_MODE = digitalRead(INPUT_MODE);  // HIGH for track, LOW for sport
 
-    // Turn buttons on sequentially
-    // LDA
+    // Press buttons sequentially (turn corresponding LED on with button press)
+
+    // LDA on
     digitalWrite(SWITCH_LDA, HIGH);
     digitalWrite(LED_LDA, HIGH);
-    delay(4000);
+    delay(3500);
+    // LDA off
     digitalWrite(SWITCH_LDA, LOW);
     digitalWrite(LED_LDA, LOW);
     delay(500);
 
-    // iMT
+    // iMT on
     digitalWrite(SWITCH_IMT, HIGH);
     digitalWrite(LED_IMT, HIGH);
     delay(500);
+    // iMT off
     digitalWrite(SWITCH_IMT, LOW);
     digitalWrite(LED_IMT, LOW);
-    delay(1000);
+    delay(500);
 
-    // Auto Stop/Start
+    // Auto stop/start on
     digitalWrite(SWITCH_ASS, HIGH);
     digitalWrite(LED_ASS, HIGH);
     delay(500);
+    // Auto stop/start off
     digitalWrite(SWITCH_ASS, LOW);
     digitalWrite(LED_ASS, LOW);
     delay(1000);
 
-    // Mode Select
-    digitalWrite(SWITCH_MODE, HIGH);
+    // Mode select
+    // Turn appropriate LED on
     if(STATE_MODE){
         digitalWrite(LED_TRACK, HIGH);
     }else{
         digitalWrite(LED_SPORT, HIGH);
     }
-    delay(500);
-    digitalWrite(SWITCH_MODE, LOW);
+    // Mash mode button 4 times to guarantee button registration
+    for(int i=0;i<4;i++){
+      digitalWrite(SWITCH_MODE, HIGH);
+      delay(500);
+      digitalWrite(SWITCH_MODE, LOW);
+      delay(500);
+    }
+    // Switch mode LEDs off
     digitalWrite(LED_TRACK, LOW);
     digitalWrite(LED_SPORT, LOW);
-    delay(1000);
+    delay(500);
 
-    // ESC
+    // ESC on (must happen after sport/track mode has been selected)
     digitalWrite(SWITCH_ESC, HIGH);
     digitalWrite(LED_ESC, HIGH);
     delay(500);
+    // ESC off, check input state to determine if short or long press required
     if(STATE_ESC){
         // If true, short press for expert mode
         digitalWrite(SWITCH_ESC, LOW);
